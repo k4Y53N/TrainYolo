156305171c911b624a2ecfac90d793ce0236a69a
k4Y53N <ks93041@gmail.com>
2021-06-06 16:24:38 +0800

    modified:   makeYoloConfig.py

--- makeYoloConfig.py
+++ makeYoloConfig.py
@@ -369,8 +369,11 @@ def Main(args):
     config['YOLO']['ANCHORS_TINY'] = anchor[1]
     config['TRAIN']['ANNOT_PATH'] = str(train_yolo_format_save_path)
     config['TRAIN']['INPUT_SIZE'] = args.size
+    config['TRAIN']['BATCH_SIZE'] = args.batch_size
     config['TEST']['ANNOT_PATH'] = str(val_yolo_format_save_path)
     config['TEST']['INPUT_SIZE'] = args.size
+    config['TEST']['BATCH_SIZE'] = args.batch_size
+
 
     if not config['pretrain']:
         config['TRAIN']['FISRT_STAGE_EPOCHS'] = 0
@@ -393,6 +396,7 @@ if __name__ == '__main__':
                         help='Frame work')
     parser.add_argument('-t', '--tiny', type=bool, default=False, help='Tiny model?')
     parser.add_argument('-p', '--pretrain', type=str, default='', help='Pretrain weight path')
+    parser.add_argument('-bs', '--batch_size', type=int, default=4, help='batch size')
     parser.add_argument('-ts', '--train_size', type=int, default=1000, help='Train epoch size')
     parser.add_argument('-vs', '--val_size', type=int, default=200, help='Val epoch size')
     args = parser.parse_args()
